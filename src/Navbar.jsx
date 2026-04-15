fbafc735f5db08d1e2a56fba03caf6ba3992df2a
Fabricio <fabricio.bizotto@ifc.edu.br>
2026-04-15 10:37:43 -0300

    asd

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,21 +1,20 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
-
-
+import ana from "./assets/img/ana.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <a href="/" className="logo">
         <div className="logo-icon">
-          <img src="/img/ana.png" alt="Logo " />
+          <img src={ana} alt="Logo " />
         </div>
         <span className="center"></span>
       </a>
 
       <div className="nav-links">
         <Link to="/galeria">Galeria</Link>
-         <Link to="/contato">Contato</Link>
+        <Link to="/contato">Contato</Link>
       </div>
     </nav>
   );
